12386674c55e818d97c041e54452753be6704bd3
AJ Javadi <amirjavadi25@gmail.com>
2023-07-05 21:12:03 -0500

    fixed bug in script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,111 +1,53 @@
-/********************
- * 
- * 
- * 
-//TODO:
-// - hide weather data until search is performed
-// - add event listener for search form 
-// add city to search history
-// 5 day forecast 
-// clear search index
- * 
- * 
- */
+// Housekeeping
+const citySearchForm = document.getElementById("city-search");
+const cityButtons = document.getElementById("city-buttons");
+const weatherCard = document.querySelector(".card");
+const forecast = document.querySelector(".card-group");
 
+// API url and key
+const apiKey = "3766791156393ba8d86e980934844d3e";
 
+//hide weather data until search
 
-// set up the buttons 
-let citySearchForm = document.querySelector("#city-search");
+// Search history
+const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
-// set up the buttons
-let  cityButtons = document.querySelector("#city-buttons");
+// add search history to buttons
+searchHistory.forEach(function (cityName) {
+    let button = document.createElement("button");
+    button.className = "btn btn-primary";
+    button.textContent = cityName;
+    button.addEventListener("click", function () {
+        citySearchForm[0].value = this.textContent;
+        citySearchForm.dispatchEvent(new Event("submit"));
+    });
+    cityButtons.appendChild(button);
 
-
-// set up the card
-let  card = document.querySelector(".card");
-
-// set up the card body 
-let  cardBody = document.querySelector(".card-group");
-
-
-// set up the weather card
-let  weatherCard = document.querySelector("#weather-card");
-
-
-// set up the forecast
-let  forecast = document.querySelector("#forecast");
-
-
-// API URL and key: 
-const APIurl = "https://api.openweathermap.org/data/2.5/weather?q=";
-
-
-const apiKey =  "3766791156393ba8d86e980934844d3e";
-
-
-
-
-//hide the weather data untill search is performed
-
-
-// search history
-
-/// Search history
-let  searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-
-
-// search the local storage for the search history
-// need to stringify the JSON
-document.getElementById("clear-history").addEventListener("click", function () {
-    searchHistory = [];
-    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-
-    while (cityButtons.firstChild) {
-        cityButtons.removeChild(cityButtons.firstChild);
-    }
 });
 
 
+// Event listener for search form
+citySearchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const cityName = event.target[0].value;
 
-
-// add search histtory buttons 
-searchHistory.forEach(function(cityName) {
-    let cityButton = document.createElement("button");
-    cityButton.className = "btn btn-secondary btn-lg btn-block";
-    cityButton.textContent = cityName;
-    // add event listener to the button
-    cityButton.addEventListener("click", function() {
-        citySearchForm[0].value = this.textContent
-        citySearchForm.dispatchEvent(new Event("submit"));
-
-    });
-    cityButtons.appendChild(cityButton);
+    // add city to search history
+    if (!searchHistory.includes(cityName)) {
+        searchHistory.push(cityName);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 
 
 
-//************************ */
-// event listener for search form
-citySearchForm.addEventListener("submit", function(event) {
-    event.preventDefault ();
-const cityName = citySearchForm[0].value;
-
-
-// add the city to the search history
-let newCityButton = document.createElement("button");
-newCityButton.className = "btn btn-secondary btn-lg btn-block";
-
-newCityButton.textContent = cityName;
-
-newCityButton.addEventListener("click", function() {
-    citySearchForm[0].value = this.textContent;
-    citySearchForm.dispatchEvent(new Event("submit"));
+        let newCityButton = document.createElement("button");
+        newCityButton.className = "btn btn-primary";
+        newCityButton.textContent = cityName;
+        newCityButton.addEventListener("click", function () {
+            citySearchForm[0].value = this.textContent;
+            citySearchForm.dispatchEvent(new Event("submit"));
         });
         cityButtons.appendChild(newCityButton);
-    });
+    };
     
-
-    // ************************ 
-
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`)
         .then(response => response.json())
@@ -154,11 +96,9 @@ cityButtons.addEventListener("input", function (event) {
 // clear search history
 document.getElementById("clear-history").addEventListener("click", function () {
     searchHistory = [];
-    localStorage.setItem("search-history", JSON.stringify(searchHistory));
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 
     while (cityButtons.firstChild) {
         cityButtons.removeChild(cityButtons.firstChild);
     }
 });
-
-// *************************
